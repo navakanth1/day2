9533dcda85261a8a81fb0154b060c20d203d0f6c
navakanth1 <navakanthreddy4445@gmail.com>
2022-03-03 11:41:55 +0530

    if elif else

---
+++ if elif else.py
@@ -0,0 +1,7 @@
+a = 100
+if a==0:
+    print("Its a 0")
+elif a>0:
+    print("Its > than 0")
+else:
+    print("Its < than 0")
---
+++ if else.py
@@ -0,0 +1,6 @@
+enter=int(input("Enter a number"))
+while a>2 & a<50:
+     if number%3==0:
+         print(number, "is odd")
+else:
+     print(number,"is odd")
